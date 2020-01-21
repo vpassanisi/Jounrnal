7e4b89b9cb65aa4e10de2a49c2d9394459ead8b3
vpassanisi <vpassanisi@gmail.com>
2020-01-20 22:29:17 -0800

    .yaml changed and environment variables reset

--- models/User.js
+++ models/User.js
@@ -45,7 +45,7 @@ UserSchema.pre("save", async function(next) {
 
 UserSchema.methods.getSignedJwtToken = function() {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE
+    expiresIn: JWT_EXPIRE
   });
 };
 
